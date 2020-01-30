5e36438b631cfb9dcc52baeeda56ff0bf2587146
gnpq1996 <60232800+gnpq1996@users.noreply.github.com>
2020-01-30 15:56:45 -0300

    blockout pronto

---
+++ QuantumG/ControleAnimacao.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleAnimacao : MonoBehaviour
+{
+    Animator anim;
+    [SerializeField] float velocidadeAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        InvokeRepeating("AlteraAnim", 1, 1);
+    }
+    
+    void alteraAnim()
+    {
+        anim.speed = velocidadeAnim;
+
+    }
+}
